af7246818e1151142be1ba022a0ccd2f25ec374b
alex-liping <157304277+alex-liping@users.noreply.github.com>
2024-01-27 21:19:55 +0100

    rozprac_II_nevyladene

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 led.enable(false)
-pins.digitalWritePin(DigitalPin.P2, 1)
+pins.digitalWritePin(DigitalPin.P2, 0)
 basic.forever(function () {
     if (pins.analogReadPin(AnalogPin.P1) > 550) {
+        serial.writeValue("cislo na pinu 1", pins.analogReadPin(AnalogPin.P1))
+        basic.pause(500)
         music.play(music.tonePlayable(698, music.beat(BeatFraction.Quarter)), music.PlaybackMode.InBackground)
         pins.digitalWritePin(DigitalPin.P2, 1)
         basic.pause(5000)
